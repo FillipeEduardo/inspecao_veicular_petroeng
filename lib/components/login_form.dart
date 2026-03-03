9e7956a420534755341b5f18e1412e265d501af0
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2026-03-03 12:46:23 -0300

    adiciona formulário de login e ajusta layout da página de login

---
+++ lib/components/login_form.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "E-mail",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:inspecao_veicular_petroeng/components/login_form.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
+    final alturaSafe =
+        mq.size.height - mq.viewPadding.top - mq.viewPadding.bottom;
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             Container(
-              height: 80,
+              height: alturaSafe * 0.1,
               decoration: BoxDecoration(
                 border: Border(
                   bottom: BorderSide(
@@ -51,6 +55,44 @@ class LoginPage extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            SizedBox(
+              height: alturaSafe * 0.25,
+              width: .infinity,
+              child: Column(
+                mainAxisAlignment: .center,
+                crossAxisAlignment: .center,
+                spacing: 12,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.all(Radius.circular(50)),
+                    ),
+                    child: Icon(Icons.shield, color: Colors.white),
+                  ),
+                  Text(
+                    "Bem-vindo de volta",
+                    style: TextStyle(fontWeight: .bold, fontSize: 25),
+                  ),
+                  Text("Entre em sua conta para continuar"),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+              padding: EdgeInsets.fromLTRB(20, 30, 10, 20),
+              height: alturaSafe * 0.55,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.primary,
+                  style: .solid,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+              ),
+              child: LoginForm(),
             ),
           ],
         ),
