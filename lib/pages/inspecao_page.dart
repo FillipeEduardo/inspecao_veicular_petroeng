8c9a77c1b9b36ad4e5379634f27c3ed617eb589e
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2026-03-12 09:59:06 -0300

    atualiza layout da página de inspeção, ajusta altura do container e modifica título para "Inspeção"

--- lib/pages/inspecao_page.dart
+++ lib/pages/inspecao_page.dart
@@ -56,11 +56,13 @@ class _InspecaoPageState extends ConsumerState<InspecaoPage> {
   @override
   Widget build(BuildContext context) {
     final listaStatusInspecaoState = ref.watch(listaStatusInspecaoProvider);
+    final mq = MediaQuery.of(context);
+    final alturaSafe = mq.size.height - mq.padding.top - mq.padding.bottom;
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 80,
         title: Text(
-          widget.inspecao.item.nome,
+          "Inspeção",
           style: TextStyle(
             color: Theme.of(context).colorScheme.primary,
             fontWeight: FontWeight.bold,
@@ -82,68 +84,85 @@ class _InspecaoPageState extends ConsumerState<InspecaoPage> {
           ),
         ],
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                spacing: 30,
-                children: [
-                  DropdownPadrao(
-                    label: "Status do Item",
-                    opcoes: listaStatusInspecaoState.statusInspecao
-                        .map((x) => {x.id: x.nome})
-                        .toList(),
-                    formState: _formState,
-                    nome: "statusInspecaoId",
-                    validacao: (value) {
-                      if (value == null) return "Selecione um status.";
-                      return null;
-                    },
-                  ),
-                  InputPadrao(
-                    label: "Observação",
-                    formState: _formState,
-                    nome: "observacao",
-                    maxLines: 5,
-                  ),
-                  Container(
-                    width: .infinity,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () => _onSubmit(listaStatusInspecaoState),
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll(
-                          Colors.transparent,
-                        ),
-                        shadowColor: WidgetStatePropertyAll(Colors.transparent),
-                        fixedSize: WidgetStatePropertyAll(Size.fromHeight(50)),
-                      ),
-                      child: Row(
-                        crossAxisAlignment: .center,
-                        mainAxisAlignment: .center,
-                        spacing: 5,
-                        children: [
-                          Text(
-                            "Continuar Inspeção",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: .bold,
-                            ),
-                          ),
-                          Icon(Icons.arrow_forward, color: Colors.white),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: alturaSafe * 0.9,
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
+              Text(
+                widget.inspecao.item.nome,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: .bold,
+                  fontSize: 30,
+                ),
               ),
-            ),
+              SizedBox(height: alturaSafe * 0.15),
+              Form(
+                key: _formKey,
+                child: Column(
+                  spacing: 30,
+                  children: [
+                    DropdownPadrao(
+                      label: "Status do Item",
+                      opcoes: listaStatusInspecaoState.statusInspecao
+                          .map((x) => {x.id: x.nome})
+                          .toList(),
+                      formState: _formState,
+                      nome: "statusInspecaoId",
+                      validacao: (value) {
+                        if (value == null) return "Selecione um status.";
+                        return null;
+                      },
+                    ),
+                    InputPadrao(
+                      label: "Observação",
+                      formState: _formState,
+                      nome: "observacao",
+                      maxLines: 5,
+                    ),
+                    Container(
+                      width: .infinity,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () => _onSubmit(listaStatusInspecaoState),
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(
+                            Colors.transparent,
+                          ),
+                          shadowColor: WidgetStatePropertyAll(
+                            Colors.transparent,
+                          ),
+                          fixedSize: WidgetStatePropertyAll(
+                            Size.fromHeight(50),
+                          ),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: .center,
+                          mainAxisAlignment: .center,
+                          spacing: 5,
+                          children: [
+                            Text(
+                              "Continuar Inspeção",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                            Icon(Icons.arrow_forward, color: Colors.white),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
