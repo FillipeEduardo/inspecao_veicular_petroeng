73757e789d60b39ac599db116b0ebc7fb3a80e53
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2026-03-20 14:50:27 -0300

    refatora ContratoService para utilizar constante pathContrato nas requisições

--- lib/services/contrato_service.dart
+++ lib/services/contrato_service.dart
@@ -7,16 +7,29 @@ import 'package:inspecao_veicular_petroeng/models/success_api_result.dart';
 
 class ContratoService {
   final Dio _dio;
+  static const String pathContrato = "/contrato";
 
   ContratoService(this._dio);
 
   Future<List<Contrato>?> obterTodos() async {
     try {
-      final response = await _dio.get("/contrato");
+      final response = await _dio.get(pathContrato);
       final result = SuccessApiResult.fromJson(response.data, (dados) {
         return (dados as List)
             .map((contrato) => Contrato.fromJson(contrato))
             .toList();
+      });
+      return result.dados;
+    } catch (_) {
+      return null;
+    }
+  }
+
+  Future<int?> criar(Map<String, dynamic> contrato) async {
+    try {
+      final response = await _dio.post(pathContrato, data: contrato);
+      final result = SuccessApiResult.fromJson(response.data, (dados) {
+        return dados as int;
       });
       return result.dados;
     } catch (_) {
